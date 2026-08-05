f8341a04741e8c5572ee8307e123bd1128bf0e6f
yunhawifood <yunhawifood@gmail.com>
2026-08-05 22:33:35 +0900

    Fix admin data supabase connection

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,6 @@
-const SUPABASE_URL = 'NEXT_PUBLIC_SUPABASE_URL=https://rbtwvemljpgnwhlzayqy.supabase.co
-';
+const SUPABASE_URL = 'https://rbtwvemljpgnwhlzayqy.supabase.co';
 
-const SUPABASE_PUBLISHABLE_KEY = 'NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_m-Uc3eR2-57P36HMP_eHsQ_F5h1tfzu';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_m-Uc3eR2-57P36HMP_eHsQ_F5h1tfzu';
 
 window.supabaseClient = window.supabase.createClient(
 
